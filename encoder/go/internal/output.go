c3c7a0dca3658c327324b0f676da2021c41a4651
Marc Auberer <marc.auberer@chillibits.com>
2021-11-12 20:51:06 +0100

    Fix bug

--- encoder/go/internal/output.go
+++ encoder/go/internal/output.go
@@ -39,32 +39,55 @@ func (o *output) PushUInt64(value uint64, len uint64) error {
 	thisBufferBitsFree := 8 - thisBufferBitsAlloc
 	nextBufferBitsAlloc := (len - thisBufferBitsFree) % 8
 	nextBufferBitsFree := 8 - nextBufferBitsAlloc
-	// Fill the rest of the old buffer
-	bitMask, err := createBitmask64ForRange(len, len-thisBufferBitsFree)
+	// Step 1: Fill the rest of the old buffer
+	posLow := len - thisBufferBitsFree
+	if thisBufferBitsFree > len {
+		posLow = 0
+	}
+	bitMask, err := createBitmask64ForRange(len, posLow)
+	fmt.Printf("Bitmask old buffer: %064b\n", bitMask)
 	if err != nil {
 		return err
 	}
-	o.buffer |= byte((value & bitMask) >> (len - thisBufferBitsFree))
-	o.bytes[o.getCurrentIndex()] = o.buffer
-	o.cursorPos += thisBufferBitsFree
+	leftShift := thisBufferBitsFree - len
+	if len >= 8 {
+		leftShift = 0
+	}
+	o.buffer |= byte((value & bitMask) << leftShift >> posLow)
+	fmt.Printf("Buffer: %08b\n", o.buffer)
 	inputCurserPos := thisBufferBitsFree
-	// Do insert steps for middle parts
-	for inputCurserPos < len-8 {
-		bitMask, err := createBitmask64ForRange(len-inputCurserPos-8, len-inputCurserPos)
+	if thisBufferBitsAlloc+len < 8 {
+		// Use same buffer for the next round
+		o.cursorPos += len
+		inputCurserPos = len
+	} else {
+		// Write buffer to output array
+		o.bytes[o.getCurrentIndex()] = o.buffer
+		o.buffer = 0
+		o.cursorPos += thisBufferBitsFree
+	}
+	// Step 2: Do insert steps for middle parts
+	if len > 8 {
+		for inputCurserPos < len-8 {
+			bitMask, err := createBitmask64ForRange(len-inputCurserPos-8, len-inputCurserPos)
+			if err != nil {
+				return err
+			}
+			inputCurserPos += 8
+			o.bytes[o.getCurrentIndex()] = byte((value & bitMask) >> (len - inputCurserPos))
+			o.cursorPos += 8
+		}
+	}
+	// Step 3: Fill the new buffer
+	if inputCurserPos < len {
+		bitMask, err = createBitmask64ForRange(nextBufferBitsAlloc, 0)
+		fmt.Printf("Bitmask end:%064b\n", bitMask)
 		if err != nil {
 			return err
 		}
-		inputCurserPos += 8
-		o.bytes[o.getCurrentIndex()] = byte((value & bitMask) >> (len - inputCurserPos))
-		o.cursorPos += 8
+		o.buffer = byte((value & bitMask) << nextBufferBitsFree)
+		o.cursorPos += nextBufferBitsAlloc
 	}
-	// Fill the new buffer
-	bitMask, err = createBitmask64ForRange(nextBufferBitsAlloc, 0)
-	if err != nil {
-		return err
-	}
-	o.buffer = byte((value & bitMask) << nextBufferBitsFree)
-	o.cursorPos += nextBufferBitsAlloc
 	return nil
 }
 
